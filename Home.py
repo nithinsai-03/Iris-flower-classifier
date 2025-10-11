db0bbfe9fde8d5162f0bd375dbc36e8f84d48726
achal22-coder <achalshrivas2020@gmail.com>
2025-10-11 13:56:36 +0530

    added separate files for pirctires of iris setosa, versicolor and virginica ad edited the Home.py aaccordingly

--- Home.py
+++ Home.py
@@ -32,15 +32,15 @@ st.markdown("Each species has distinct physical characteristics, which allows th
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("https://raw.githubusercontent.com/allisonhorst/palmerpenguins/main/man/figures/lter_image.png", caption="Iris Setosa", use_container_width=True)
+    st.image("iris_setosa.webp", caption="Iris Setosa", use_container_width=True)
     st.markdown("**Iris Setosa:** Characterized by shorter and wider petals and sepals. Generally the easiest species to separate from the other two.")
 
 with col2:
-    st.image("https://upload.wikimedia.org/wikipedia/commons/4/41/Iris_versicolor_3.jpg", caption="Iris Versicolor", use_container_width=True)
+    st.image("iris_versicolor.jpeg", caption="Iris Versicolor", use_container_width=True)
     st.markdown("**Iris Versicolor:** Has intermediate measurements. Its petal and sepal dimensions often fall between those of Setosa and Virginica.")
 
 with col3:
-    st.image("https://upload.wikimedia.org/wikipedia/commons/9/9f/Iris_virginica.jpg", caption="Iris Virginica", use_container_width=True)
+    st.image("iris_virginica.jpeg", caption="Iris Virginica", use_container_width=True)
     st.markdown("**Iris Virginica:** Typically has the longest and widest petals and sepals among the three species.")
 
 st.markdown("""
